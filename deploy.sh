38b6ecd0ffdee0ed9350d5e320061426db7ce67c
13roky <boqiang6450@outlook.com>
2021-05-13 09:15:45 +0800

    update contents

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:13roky/Vuepress_Java.git master:gh-pages
+git push -f git@github.com:13roky/13rokyDocs.git master:gh-pages
 
 cd -
